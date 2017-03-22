23695fc8f61a6f698f6fdf6a98f066652f9f312d
ungaminga <ungaminga@protonmail.com>
2017-03-22 04:21:09 +0200

    Added sorting order for files, custom behavour for single file directory and custom column count for dirs w/o subdirs.

--- sound_gallery.py
+++ sound_gallery.py
@@ -57,7 +57,7 @@ span.play {
 
 /* A div for links */
 div.links{
-    right:0px;
+    right:5px;
     position:absolute;
     background-color: khaki;
     border: green 2px solid;
@@ -127,13 +127,36 @@ def generate_html():
 class Directory(list):
     def __init__(self, name):
         self.name = name
+        self.single_file = False
+        self.no_subdirs = True
+
+def custom_sort (to_sort):
+    list1 = []
+    list2 = []
+    for el in to_sort:
+        if el in sorting_order:
+            list1.append(el)
+        else:
+            list2.append(el)
+
+    list1.sort(key=lambda v:sorting_order.index(v))
+    list2.sort()
+
+    return list1+list2
 
 # Recursive function to generate list of directory tree
 # returns []
 def generate_tree_dirs(dir, deep=False, subdir = []):
     dirs = subdir
     ld = os.listdir(dir)
-    ld.sort()
+    ld = custom_sort(ld)
+
+    # single file in directory, must process only if dir.wav = dir
+    if deep == True and len(ld) == 1:
+        F = ld[0]
+        if re.search("..*", F) and re.split("\..*", F)[0] == subdir.name:
+            dirs.single_file = True
+
     for F in ld:
         if re.search("(.git|.py|.html|.css)", F):
             continue
@@ -144,35 +167,54 @@ def generate_tree_dirs(dir, deep=False, subdir = []):
             new_F = dir+os.sep+F
         if os.path.isdir(new_F):
             dirs.append(generate_tree_dirs(new_F, True, Directory(F)))
+            if type(subdir) is Directory:
+                subdir.no_subdirs = False
+
             continue
         dirs.append(new_F)
     return dirs
+
+def check_bigdir_for_new_div(tree, i):
+    if not(len(tree) - i > 2):
+        return False
+
+    if type(tree) is Directory and tree.no_subdirs == True:
+        return i % (len(tree)//(columns_no_subdir - 1)) == 0;
+
+    return i % (len(tree)//(columns-1)) == 0;
 
 # Recurisve function to generate <ul><li> of directories and files from tree
 # returns []
 def process_tree_dirs(tree):
     global body
     global label_num
+    global checked
     
     big = len(tree) > 20
     i = 0
     for itr in tree:
-        if big and ((i % (len(tree)//columns) == 0)):
+        if big and check_bigdir_for_new_div(tree, i):
             if i == 0:
                 body += '<div class="big">\n<div>\n'
             else:
                 body += '</div><div>\n'
 
         if type(itr) is Directory:
-            if i == 0:
+            if itr.single_file == True:
+                 process_tree_dirs(itr);
+                 i+=1
+                 continue;
+
+            if checked == True:
                 body += '<li><input type="checkbox" id="dir%i" checked = "checked" /> <label for="dir%i">'%(label_num, label_num)           
             else:
                 body += '<li><input type="checkbox" id="dir%i" /> <label for="dir%i">'%(label_num, label_num)
             body += '<span class="unchecked">&#10149;</span><span class="checked">&#8863; </span>%s</label> <ul>\n'%(itr.name)
             label_num+=1
             process_tree_dirs(itr)
+            checked = False
             body += '</ul></li>\n'
-        else:
+        else: #if type(itr) is Directory:
             short = re.split("^.*\/", itr)[1]
             body += '<li class="file"><a onclick=\'play(this)\' file="%s">'%(itr)
             body += '<span class="play">&#9658</span>%s</a>'%(short)
@@ -194,9 +236,20 @@ if __name__ == "__main__":
     </div>
 <ul>
     """
-    
+
+    sorting_order = [
+    "ru_cards_sound",
+    "ru_portrait_shout",
+    "en_cards_sound",
+    "en_portrait_shout",
+    "card_nonloc",
+    "ambience",
+    "ui"]
+
     label_num = 0
-    columns = 8
+    checked = True
+    columns = 9
+    columns_no_subdir = columns - 3
     title = "TES:L Sounds Gallery"
     tree = generate_tree_dirs(scan_dir, scan_dir!="")
     process_tree_dirs(tree)
